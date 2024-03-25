326783a019bdeab4068d01cafa60d538527cfffd
krumplimalac <hunyadyzsombor@gmail.com>
2024-03-25 18:44:43 +0100

    tests build ok

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,49 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
+    TrainController mockTC;
+    TrainUser mockTU;
+    TrainSensorImpl trainSensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        mockTC = mock(TrainController.class);
+        mockTU = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockTC, mockTU);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void CheckSpeedLimitChange() {
+        int speedLimit = 20;
+        trainSensor.overrideSpeedLimit(speedLimit);
+        verify(mockTC,times(1)).setSpeedLimit(speedLimit);
+    }
+
+    @Test
+    public void CheckSpeedLimitOverMax() {
+        int speedLimit = 600;
+        trainSensor.overrideSpeedLimit(speedLimit);
+        verify(mockTU,times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void CheckSpeedLimitMinSpeed() {
+        int speedLimit = -10;
+        trainSensor.overrideSpeedLimit(speedLimit);
+        verify(mockTU,times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void CheckSpeedLimitMiddle() {
+        int speedLimit = 10;
+        trainSensor.overrideSpeedLimit(speedLimit);
+        verify(mockTU,times(0)).setAlarmState(true);
     }
 }
